a05381fd275187d95bbd51d02fbb27ba470a5b83
karkikamal098 <kamaljungkarki13579@gmail.com>
2024-06-29 00:50:06 +0545

    some changes

--- index.js
+++ index.js
@@ -1,21 +1,27 @@
-const express= require("express")
-const cors= require("cors")
-const {connect} = require("mongoose")
+const express = require('express');
+const cors = require('cors');
+const {connect} = require('mongoose');
+
 require('dotenv').config()
 
-const userRoutes= require("./routes/userRoutes")
-const postRoutes= require("./routes/postRoutes")
+const userRoutes = require('./routes/userRoutes');
+const postRoutes = require('./routes/postRoutes');
+
 
 const app = express();
-app.use(express.json({extended: true}));
+
+app.use(express.json({extended:true}));
 app.use(express.urlencoded({extended:true}));
-app.use(cors({credentials: true, origin:"http://localhost:3000"}));
+app.use(cors({credentials:true,origin:"https://locahost:3000"}));
+
+app.use('/api/users',userRoutes);
+app.use('/api/posts',postRoutes);
+
+connect(process.env.MONGO_URI).then(app.listen(3000,()=>{console.log(`server is running on the port ${process.env.PORT}`)})).catch(error=>{console.log(error)});
 
 
-app.use('/api/users', userRoutes);
-app.use("/api/posts", postRoutes);
 
-connect(process.env.MONGO_URI).then(app.listen(3000, () => {
-    console.log(`Server is running on port ${process.env.PORT}`);
-  })).catch(error=>{console.log(error)});
+
+
+
 
