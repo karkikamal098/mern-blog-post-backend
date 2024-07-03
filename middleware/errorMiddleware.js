6125e209fbed283d798883fde203751654fdd51c
karkikamal098 <kamaljungkarki13579@gmail.com>
2024-07-03 23:31:09 +0545

    some changes

--- middleware/errorMiddleware.js
+++ middleware/errorMiddleware.js
@@ -11,7 +11,7 @@ const errorHandler = (error, req, res, next) => {
     return next(error);
   }
   res
-    .status(error.code || 500)
+    .status(error.code || 510)
     .json({ message: error.message || "An unknown error occurred" });
 };
 
