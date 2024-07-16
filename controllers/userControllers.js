bc4aec5d87fc5414a84666d6590c8397a42cfe6f
karkikamal098 <kamaljungkarki13579@gmail.com>
2024-07-17 00:03:43 +0545

    register user backend with bcrypt

--- controllers/userControllers.js
+++ controllers/userControllers.js
@@ -2,87 +2,90 @@
 // POST = api/users/post
 //Unprotected
 
+const User = require("../models/userModel")
+const HttpError= require("../models/errorModel");
+
 const bcrypt = require('bcryptjs');
 
-const User = require("../models/userModel");
-const HttpError = require('../models/errorModel')
+const registerUsers = async (req,res,next) => {
+    try{
+    const {name,email,password,password2}=req.body;
 
-const registerUsers = async (req,res,next)=>{
-    try {
-
-        const {name,email,password,password2} = req.body;
-        if (!name || !email || !password) {
-            return next(new HttpError("fill in all fields"))
-        }
-
-        const newEmail = email.toLowerCase()
-
-        const emailExists = await User.findOne({ email: newEmail })
-
-        if (emailExists) {
-            return next(new HttpError("email already exists", 409));
-        }
-
-        if (password.trim().length < 8) {
-            return next(new HttpError("password should be at least 8 characters long", 400));
-        }
-
-        if (password != password2) {
-            return next(new HttpError("passwords do not match", 400));
-        }
-
-        const salt = await bcrypt.genSalt(10)
-        const hashedPass = await bcrypt.hash(password, salt)
-        const newUser = await User.create({name, email: newEmail, password:hashedPass})
-        res.status(201).json(newUser)
-    
-    } catch (error) {
-        return next(new HttpError("user registration failed", 422));
+    if(!name || !email || !password || !password2){
+       return next(new HttpError("please enter all the fields"),400);
     }
+
+    if(password !== password2){
+        return next(new HttpError("please enter the both password same"),400);
+    }
+    
+    const newEmail = email.toLowerCase();
+    const newExist = await User.findOne({email:newEmail});
+    if(newExist){
+        return next(new HttpError("User already existed",400));
+    }
+
+    if(password.trim().length<6){
+        return next(new HttpError("please enter a little longer password",400));
+    }
+
+    const salt = await bcrypt.genSalt(10);
+    const hashpassword = await bcrypt.hash(password, salt);
+    const newUser= await User.create({
+       name,
+       email: newEmail,
+       password: hashpassword
+    })
+    res.status(202).json(`{email} is registered`);
+    }
+
+    catch(error){
+        return next(new HttpError("user registration failed", 500));
+    }
+
 }
 
 
 // ===Login new User
 // POST = api/users/login
 //Unprotected
-const loginUsers = async (req,res,next)=>{
-    res.json("login user")
-}
-
+const loginUsers = async (req, res, next) => {
+  res.json("login user");
+};
 
 // ===user profile
 // POST = api/users/userprofile
 //protected
-const getUser = async (req,res,next)=>{
-    res.json("get user information")
-}
-
+const getUser = async (req, res, next) => {
+  res.json("get user information");
+};
 
 // ===change user Avatar
 // POST = api/users/change-avatar
 //protected
-const changeAvatar = async (req,res,next)=>{
-    res.json("change avatar")
-}
+const changeAvatar = async (req, res, next) => {
+  res.json("change avatar");
+};
 
 // ===edit user details
 // POST = api/users/edit-User
 //protected
-const editUser = async (req,res,next)=>{
-    res.json("edit profile")
-}
-
+const editUser = async (req, res, next) => {
+  res.json("edit profile");
+};
 
 // ===get authors
 // POST = api/users/authors
 //protected
-const getAuthors = async (req,res,next)=>{
-    res.json("get authors details")
-}
+const getAuthors = async (req, res, next) => {
+  res.json("get authors details");
+};
 
-
-module.exports = {registerUsers, loginUsers, getUser, changeAvatar, editUser, getAuthors}
-
-
-
-
+module.exports = {
+  registerUsers,
+  loginUsers,
+  getUser,
+  changeAvatar,
+  editUser,
+  getAuthors,
+};
